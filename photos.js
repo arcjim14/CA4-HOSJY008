e5b8b357fd240413c9d1e8666fc0f235f533c4b6
arcjim14 <87861300+arcjim14@users.noreply.github.com>
2021-08-12 22:50:36 +0800

    Add files via upload

--- photos.js
+++ photos.js
@@ -1,3 +1,23 @@
 var qrpardalote = document.createElement('img');
 qrpardalote.src = "http://www.outgrabe.net/bird00.jpg";
 document.body.appendChild(qrpardalote)
+
+
+var qrPurpleSwampHen = document.createElement('img');
+qrPurpleSwampHen.src = "http://www.outgrabe.net/bird01.jpg";
+document.body.appendChild(qrPurpleSwampHen)
+
+
+var qrWhiteheadedstilt = document.createElement('img');
+qrWhiteheadedstilt.src = "http://www.outgrabe.net/bird02.jpg";
+document.body.appendChild(qrWhiteheadedstilt)
+
+
+var qrInlandThornbill = document.createElement('img');
+qrInlandThornbill.src = "http://www.outgrabe.net/bird03.jpg";
+document.body.appendChild(qrInlandThornbill)
+
+
+var qrRoseRobin = document.createElement('img');
+qrRoseRobin.src = "http://www.outgrabe.net/bird04.jpg";
+document.body.appendChild(qrRoseRobin)
